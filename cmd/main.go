b181e62c24668657ef7bbebd08e18159a8591393
Tom Tooley <tomtooley2006@gmail.com>
2026-05-23 20:18:15 +0100

    add bare HTTP server with Chi router and health check

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("server running"))
+	})
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tom-0406/Betty
+
+go 1.26.3
+
+require github.com/go-chi/chi/v5 v5.3.0 // indirect
